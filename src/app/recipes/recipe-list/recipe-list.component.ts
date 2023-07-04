a11f793ab66be2cb0a89a516478c6ba2ddbe13bb
Hassan Aijaz <hassanaijaz233@gmail.com>
2023-07-04 23:21:58 +0500

    Add recipe mode and update html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'Description for recipe 1', 'https://c.pxhere.com/images/22/4a/6321d069b3386c120e0c860dc56a-1620832.jpg!d'),
+    new Recipe('Recipe 2', 'Description for recipe 2', 'https://c.pxhere.com/images/22/4a/6321d069b3386c120e0c860dc56a-1620832.jpg!d'),
+    new Recipe('Recipe 3', 'Description for recipe 3', 'https://c.pxhere.com/images/22/4a/6321d069b3386c120e0c860dc56a-1620832.jpg!d')
+  ];
 
 }
